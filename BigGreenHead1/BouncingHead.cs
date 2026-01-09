750b98ae64bac711b01909e5a1867b7267b34972
dagroup2017 <dagroup2017@gmail.com>
2026-01-08 19:56:05 -0800

    FinalBouncingHead

--- BigGreenHead1/BouncingHead.cs
+++ BigGreenHead1/BouncingHead.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +11,65 @@ namespace BigGreenHead1
 {
     public class BouncingHead
     {
-        public Vector2 Position;
-        public Vector2 Velocity;
-        public Texture2D Texture;
-
-        public BouncingHead(Vector2 position, Vector2 velocity, Texture2D texture)
+        //public Vector2 Velocity;
+        // public Vector2 Position;
+        public static Texture2D Texture;
+        public static Viewport GraphicsViewport;
+        
+        // using private to enforce encapsulation
+        private Vector2 position;
+        private Vector2 velocity;
+        
+        // accessors and mutators
+        public Vector2 Position
         {
-            Position =  new Vector2(0f,0f);
-            Velocity = velocity;
-            Texture = texture;
+            get { return position; }
+            set { position = value; }
+        }
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+        
+        private Color drawingColor;
+        public Color DrawingColor
+        {
+            get { return drawingColor; }
+            set { drawingColor = value; }
+        }
+        
+        
+        
+        
+      public BouncingHead(float x, float y, float vx, float vy)
+        {
+            position =  new Vector2(x, y);
+            velocity = new Vector2(vx, vy);
+            drawingColor = Color.White;
+        }
+      
+      public BouncingHead()
+      {
+        position = new Vector2(0f, 0f);
+        velocity = new Vector2(1f, 1f);
+        drawingColor = Color.White;
+      }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, position, drawingColor);
+        }
+        
+        public void Update()
+        {
+            position += velocity;
+            
+            if (position.X < 0 || position.X > GraphicsViewport.Width - Texture.Width)
+                velocity.X = -velocity.X;
+            
+            if (position.Y < 0 || position.Y > GraphicsViewport.Height - Texture.Height)
+                velocity.Y = -velocity.Y;
         }
     }
 }
--- BigGreenHead1/Game1.cs
+++ BigGreenHead1/Game1.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+using FxResources.System;
 
 namespace BigGreenHead1;
 
@@ -8,7 +12,18 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager graphics;
     private SpriteBatch _spriteBatch;
- 
+
+    private Texture2D background;
+    Rectangle backgroundRectangle;
+    
+    private Song song;  
+    private BouncingHead head1, head2, head3;
+    private BouncingHead[] heads;
+    int headCount = 10;
+
+    private Random r = new Random();
+    
+    
     Texture2D spriteTexture;
 
     Vector2 spriteLocation = new Vector2(0f, 0f);
@@ -27,7 +42,25 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        // head1 = new BouncingHead(0,0,1,1);
+        // head2 = new BouncingHead(200,200,4,4);
+        // head2.DrawingColor = Color.Teal;
+        // head3 = new BouncingHead();
+        // head3.Velocity = new Vector2(10f, 10f); 
+        // head3.DrawingColor = new Color(255, 50, 123);
 
+        // instantiate an array of heads
+        heads = new BouncingHead[headCount];
+        for (int i = 0; i < headCount; i++)
+        {
+            heads[i] = new BouncingHead();
+            heads[i].Velocity = new Vector2(r.Next(2, 11), r.Next(2, 11));
+            int rd = r.Next(200, 256);
+            int g = r.Next(0, 256);
+            int b = r.Next(150, 256);
+            heads[i].DrawingColor = new Color(rd, g, b);
+        }
+        
         base.Initialize();
     }
 
@@ -36,7 +69,15 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        spriteTexture = Content.Load<Texture2D>("greenhead");
+        // spriteTexture = Content.Load<Texture2D>("greenhead");
+        BouncingHead.GraphicsViewport = GraphicsDevice.Viewport;
+        BouncingHead.Texture = Content.Load<Texture2D>("greenhead");
+        
+        background = Content.Load<Texture2D>("sjb");
+        backgroundRectangle = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+      
+        song = Content.Load<Song>("wakingup");
+        MediaPlayer.Play(song);
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,16 +89,36 @@ public class Game1 : Game
         // spriteLocation.X += 1f;
         // spriteLocation.Y += 0.5f;
 
-        spriteLocation += SpriteVelocity;
+        // spriteLocation += SpriteVelocity;
+        //
+        // if (spriteLocation.X > graphics.GraphicsDevice.Viewport.Width - spriteTexture.Width || spriteLocation.X < 0)
+        // {
+        //     SpriteVelocity.X *= -1;
+        // }
+        //
+        // if (spriteLocation.Y > graphics.GraphicsDevice.Viewport.Height - spriteTexture.Height || spriteLocation.Y < 0)
+        // {
+        //     SpriteVelocity.Y *= -1;
+        // }
+        // head1.Position += head1.Velocity;
+        //
+        // if (head1.Position.X > graphics.GraphicsDevice.Viewport.Width - spriteTexture.Width || head1.Position.X < 0)
+        // {
+        //     head1.Velocity.X = - head1.Velocity.X;
+        // }
+        //
+        // if (head1.Position.Y > graphics.GraphicsDevice.Viewport.Height - spriteTexture.Height || head1.Position.Y < 0)
+        // {
+        //     head1.Velocity.Y = - head1.Velocity.Y;
+        // }
         
-        if (spriteLocation.X > graphics.GraphicsDevice.Viewport.Width - spriteTexture.Width || spriteLocation.X < 0)
-        {
-            SpriteVelocity.X *= -1;
-        }
+        // head1.Update();
+        // head2.Update(); 
+        // head3.Update();
         
-        if (spriteLocation.Y > graphics.GraphicsDevice.Viewport.Height - spriteTexture.Height || spriteLocation.Y < 0)
+        foreach (BouncingHead head in heads)
         {
-            SpriteVelocity.Y *= -1;
+            head.Update();
         }
         
         base.Update(gameTime);
@@ -69,8 +130,14 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.Draw(spriteTexture, spriteLocation, Color.White);
-        _spriteBatch.End();
+        // _spriteBatch.Draw(spriteTexture, head1.Position, Color.White);
+        _spriteBatch.Draw(background, backgroundRectangle, Color.White);
+        foreach (BouncingHead head in heads)
+        {
+            head.Draw(_spriteBatch);
+        }
+
+    _spriteBatch.End();
 
         base.Draw(gameTime);
     }
